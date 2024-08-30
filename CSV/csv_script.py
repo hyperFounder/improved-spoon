6cb265025a34d138b8b1bb042839e9297a1c4f5c
Ryan Ribeiro <117395661+hyperFounder@users.noreply.github.com>
2024-08-31 00:50:03 +0100

    Update csv_script.py

--- CSV/csv_script.py
+++ CSV/csv_script.py
@@ -47,10 +47,10 @@ selected_assets = [
 process_all = input("Deseja processar todos os ativos? (yes/no): ").strip().lower() in ['y', 'yes']
 
 if process_all:
-    output_file = 'export_jumba.gamma.csv'
+    output_file = 'export.csv'
     process_file(input_file, output_file)
 else:
-    output_file = 'export_jumba.gamma.csv'
+    output_file = 'export.csv'
     process_file(input_file, output_file, selected_assets)
 
 print(f'Arquivo CSV gerado: {output_file}')
